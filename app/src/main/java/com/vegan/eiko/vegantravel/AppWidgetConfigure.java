d66ccc2cd3bc275714d7ca046349f0729d05a49a
econMinnie <etcontest7@gmail.com>
2019-07-27 22:56:59 -0700

    updated 7.27 tryied to add widget

---
+++ app/src/main/java/com/vegan/eiko/vegantravel/AppWidgetConfigure.java
@@ -0,0 +1,6 @@
+package com.vegan.eiko.vegantravel;
+
+import android.app.Activity;
+
+public class AppWidgetConfigure extends Activity {
+}
---
+++ app/src/main/java/com/vegan/eiko/vegantravel/AppWidgetProvider.java
@@ -0,0 +1,28 @@
+package com.vegan.eiko.vegantravel;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class AppWidgetProvider extends android.appwidget.AppWidgetProvider {
+@Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager
+    , int[] appWidgetIds){
+        final int N = appWidgetIds.length;
+
+//        for (int i=0; i<N; i++){
+//            int appwideget= appWidgetIds[1];
+        for (int appwideget : appWidgetIds){
+            //For Intent to launch app(Mainactivity).
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                    0, intent, 0 );
+            RemoteViews views = new RemoteViews(context.getPackageName(),
+                    R.layout.appwidget);
+            views.setOnClickPendingIntent(R.id.widget_imageView, pendingIntent);
+            appWidgetManager.updateAppWidget(appwideget, views);
+        }
+    }
+}
