e7495662bb999e0be653e71fc6a7b43ed56515f0
eiko <ohnoteiko@gmail.com>
2023-06-07 17:01:29 -0400

    updated 4.26 part2

--- app/src/main/java/com/vegan/eiko/vegantravel/Veg_filipino.java
+++ app/src/main/java/com/vegan/eiko/vegantravel/Veg_filipino.java
@@ -1,8 +1,8 @@
 package com.vegan.eiko.vegantravel;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 /**
  * Created by eiko on 11/24/2015.
